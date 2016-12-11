1cae6fb45a1d9290e46d8c9c72b1df80fe06bf14
Andrey Avol Volynkin <mail.avol@gmail.com>
2016-12-11 13:18:38 +0300

    Removed build files from git.

--- server/utils/FsAsync.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-;
-const Fs = require("fs");
-const promisify_1 = require("./promisify");
-const readFile = promisify_1.default(Fs.readFile);
-exports.readFile = readFile;
-const writeFile = promisify_1.default(Fs.writeFile);
-exports.writeFile = writeFile;
