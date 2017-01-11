3da636e3d7f2b5209398fdb7c31c2b6d0ae47ac2
Andrey Avol Volynkin <mail.avol@gmail.com>
2017-01-11 22:01:43 +0300

    Set to use auth for `/api` only
    Now session and authentication used only for `/api` requests.
    
    Added handler for 404 errors.
    
    Added `debug` script to npm for run server in debug mode.

--- server.src/index.ts
+++ server.src/index.ts
@@ -31,16 +31,27 @@ if ( app.get( 'env' ) === 'production' )
 }
 
 app.use( Express.static( Path.resolve( rootPath, 'public' ) ) );
-app.use( sessionMiddleware );
-app.use( accountMiddleware );
-app.use( authMiddleware );
+app.use( '/api', sessionMiddleware );
+app.use( '/api', accountMiddleware );
+app.use( '/api', authMiddleware );
 app.use( '/api', apiRouter );
 
-app.get(
-	'/test',
+app.use(
 	( _request: Express.Request, response: Express.Response ) =>
 	{
-		response.send( 'Hello World!' );
+		response.status( 404 );
+		
+		response.format(
+			{
+				'text/plain': () => response.send( 'Not found' ),
+				'text/html': () => response.sendFile(
+					Path.resolve( rootPath, 'views/static/404.html' ),
+				),
+				'application/json': () => response.send(
+					{error: 'Not found'},
+				),
+			},
+		);
 	},
 );
 
